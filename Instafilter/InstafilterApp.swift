1c4761abb3f6b87e2300a20dbf2e0899b76a86ce
Marcus Benoit <benoit.marcus@gmail.com>
2024-06-13 14:23:51 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Marcus Benoit on 13.06.24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
